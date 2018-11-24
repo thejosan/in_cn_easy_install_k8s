69ebe71cfdcda9c3fbc9bec93855e6e6e7ec86d9
thejosan <33949813+thejosan@users.noreply.github.com>
2018-11-24 17:28:13 +0800

    Update install-master.sh

--- install-master.sh
+++ install-master.sh
@@ -23,11 +23,11 @@ echo -e "\033[32m## 安装并初始化master ===================================
 if [ ! -n "$1" ] ;then
 	echo "kubeadm init --kubernetes-version=v$K8sVersion --pod-network-cidr=10.244.0.0/16"
 	echo "Please wait a few minutes!"
-	sudo kubeadm init --kubernetes-version=v$K8sVersion --pod-network-cidr=10.244.0.0/16 > init.log
+	/bin/bash kubeadm init --kubernetes-version=v$K8sVersion --pod-network-cidr=10.244.0.0/16 > init.log
 else
         echo "kubeadm init --kubernetes-version=v$K8sVersion --apiserver-advertise-address $1 --pod-network-cidr=10.244.0.0/16" 
         echo "Please wait a few minutes!"
-	sudo kubeadm init --kubernetes-version=v$K8sVersion --apiserver-advertise-address $1 --pod-network-cidr=10.244.0.0/16 > init.log
+	/bin/bash kubeadm init --kubernetes-version=v$K8sVersion --apiserver-advertise-address $1 --pod-network-cidr=10.244.0.0/16 > init.log
 fi
 
 JoinCommand=`grep "kubeadm join" init.log`
@@ -46,7 +46,7 @@ if [ $? -eq 0 ]; then
 
     echo -e "\033[32m## 生成node节点安装包 =====================================================================\033[0m"
     mkdir -p /tmp/k8s-node-install
-    cp setupconfig.sh install-docker.sh pull-docker.sh install-k8s.sh install-node.sh /tmp/k8s-node-install
+    cp setupconfig.sh install-docker.sh pull-docker.sh install-k8s.sh install-node.sh kubeadm /tmp/k8s-node-install
     cd /tmp
     tar -czf k8s-node-install.tar.gz k8s-node-install
     sudo mv k8s-node-install.tar.gz /root/
